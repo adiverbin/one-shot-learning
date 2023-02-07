cae3fed9e1baa8a863a5609cacd3a02249dda42e
adiverbin <88194518+adiverbin@users.noreply.github.com>
2023-02-07 13:47:31 +0200

    Update train_network.py

--- train_network.py
+++ train_network.py
@@ -5,10 +5,10 @@ from siamese_network import SiameseNetwork
 
 def train():
     train_dataloader = DataLoader(LFWADataset(data_dir='lfwa/lfw2/lfw2', source_path='lfwa/lfw2/splits/train.txt'),
-                            batch_size=16, shuffle=True, num_workers=0)
-    trainer = pl.Trainer(max_epochs=200, log_every_n_steps=1, limit_train_batches=1)
-    model = SiameseNetwork()
-    trainer.fit(model, train_dataloaders=train_dataloader, )
+                            batch_size=16, shuffle=False, num_workers=0)
+    trainer = pl.Trainer(max_epochs=1000, log_every_n_steps=20, accelerator="gpu", devices=1, limit_train_batches=1)
+    model = SiameseNetwork(lr=1e-3)
+    trainer.fit(model, train_dataloaders=train_dataloader)
 
 if __name__ == '__main__':
     train()
